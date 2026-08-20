2c0cee71778ec21cfceb636650b68631ba9353aa
Uriel Maldonado <uriel@smpllabs.io>
2026-08-20 13:20:12 -0500

    Fix stale homepage assertion (#9199)

--- test/svelte_app_web/controllers/page_controller_test.exs
+++ test/svelte_app_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule UrielmWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Master"
+
+    assert html_response(conn, 200) =~ ~s(id="home-hero")
   end
 end
